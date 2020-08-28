62d13c31b3fc1ce7c3ac473bae992dda0bd1c2d0
Kushagra Gupta <gupta.kushagra3@gmail.com>
2020-08-28 19:12:23 +0530

    enabled css modules

--- src/UI/UI.js
+++ src/UI/UI.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import ContactsList from './LeftPanel/ContactsList';
 import IndividualChat from './RightPanel/IndividualChat';
+import classes from './UI.module.css';
 export default class UI extends Component {
     render() {
         return (
-            <div>
+            <div className={classes.Layout}>
                 <ContactsList/>
                 <p>This is ur chat ui</p>
                 <IndividualChat/>
